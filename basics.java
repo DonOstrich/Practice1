caff11d80448e6ce1fc6b04d7cd0b97bbb5dd33c
DonOstrich <39230452+DonOstrich@users.noreply.github.com>
2019-09-08 18:11:31 -0500

    Add files via upload

---
+++ basics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class basics {
+    public static void main(String args[]){
+        int j = 1 , k = 0 ,d = 2;
+        int addAll = j + k + d;
+        System.out.println("Hi new PC ");
+        System.out.println("add all = " + addAll);
+    }
+
+
+}
